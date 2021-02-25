69299efc521689672ed8f5a8df1cb6f268ae2783
zhukov@etalon48.com <zhukov@etalon48.com>
2021-02-25 10:51:06 +0300

    Create dashboard.vue

---
+++ src/views/dashboard.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+  <v-navigation-drawer app temporary>
+    <!-- -->
+  </v-navigation-drawer>
+
+  <v-app-bar app>
+    <!-- -->
+  </v-app-bar>
+
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+
+    <!-- Provides the application the proper gutter -->
+    <v-container fluid>
+
+      <!-- If using vue-router -->
+      <router-view></router-view>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+    <!-- -->
+  </v-footer>
+</v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      drawer: null
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
+
