0a940df18c2683219cc54803ee50c17926df1610
zhukov@etalon48.com <zhukov@etalon48.com>
2021-02-25 10:52:05 +0300

    Create login.vue

---
+++ src/views/login.vue
@@ -0,0 +1,71 @@
+<template>
+<div>
+ <v-content >
+      <v-container fill-height >
+        <v-layout align-center justify-center>
+          <v-flex xs6 sm8 md4 xs3-offset >
+            <v-card class="elevation-12">
+              <v-toolbar dark color="primary">
+                <v-toolbar-title>Login form</v-toolbar-title>
+                <v-spacer></v-spacer>
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn :href="source" icon large target="_blank" v-on="on">
+                      <v-icon large>code</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Source</span>
+                </v-tooltip>
+                <v-tooltip right>
+                  <template v-slot:activator="{ on }">
+                    <v-btn icon large href="https://codepen.io/johnjleider/pen/wyYVVj" target="_blank" v-on="on">
+                      <v-icon large>mdi-codepen</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Codepen</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form @submit.prevent>
+                  <v-text-field prepend-icon="mdi-login" name="email" label="email" type="email"></v-text-field>
+                  <v-text-field id="password" prepend-icon="mdi-lock" name="password" label="Password" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="danger" @click="login()">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      loginForm: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+  props: {
+      source: String
+    },
+  methods: {
+  login() {
+    this.$store.dispatch('login', {
+      email: this.loginForm.email,
+      password: this.loginForm.password
+    })
+  }
+}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
