842e4dcd992c2c8304c00f97e7be61d3b28da52c
zhukov@etalon48.com <zhukov@etalon48.com>
2021-02-25 10:51:28 +0300

    Create index.js

---
+++ src/store/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+import router from '../router/index'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    userProfile: {}
+  },
+  mutations: {
+    setUserProfile(state, val) {
+      state.userProfile = val
+    }
+  },
+  actions: {
+    async login({ dispatch }, form) {
+      // sign user in
+      const { user } = await firebase.auth.signInWithEmailAndPassword(form.email, form.password)
+  
+      // fetch user profile and set in state
+      dispatch('fetchUserProfile', user)
+    },
+    async fetchUserProfile({ commit }, user) {
+      // fetch user profile
+      const userProfile = await firebase.usersCollection.doc(user.uid).get()
+  
+      // set user profile in state
+      commit('setUserProfile', userProfile.data())
+      
+      // change route to dashboard
+      router.push('/')
+    }
+  },
+  modules: {
+  }
+})
